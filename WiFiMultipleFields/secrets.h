c179e54e79feeba0011d5f8b14c99af162432b57
Alperen <ozkayaal@mef.edu.tr>
2023-06-30 23:47:40 +0300

    README update

--- WiFiMultipleFields/secrets.h
+++ WiFiMultipleFields/secrets.h
@@ -1,7 +1,7 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_SSID "MySSID"	// replace MySSID with your WiFi network name
 #define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 0000000		// replace 0000000 with your channel number
